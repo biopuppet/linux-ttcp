5548ef33125ebb929ff68e39682db8daa759ff4c
Demard <biopuppet@outlook.com>
2020-06-26 13:14:43 +0800

    Fix extra stream alloc skb

--- net/ipv4/ttcp.c
+++ net/ipv4/ttcp.c
@@ -506,31 +506,6 @@ ssize_t ttcp_splice_read(struct socket *sock, loff_t *ppos,
 }
 EXPORT_SYMBOL(ttcp_splice_read);
 
-struct sk_buff *sk_stream_alloc_skb(struct sock *sk, int size, gfp_t gfp)
-{
-	struct sk_buff *skb;
-
-	/* The TTCP header must be at least 32-bit aligned.  */
-	size = ALIGN(size, 4);
-
-	skb = alloc_skb_fclone(size + sk->sk_prot->max_header, gfp);
-	if (skb) {
-		if (sk_wmem_schedule(sk, skb->truesize)) {
-			/*
-			 * Make sure that we have exactly size bytes
-			 * available to the caller, no more, no less.
-			 */
-			skb_reserve(skb, skb_tailroom(skb) - size);
-			return skb;
-		}
-		__kfree_skb(skb);
-	} else {
-		sk->sk_prot->enter_memory_pressure(sk);
-		sk_stream_moderate_sndbuf(sk);
-	}
-	return NULL;
-}
-
 static unsigned int ttcp_xmit_size_goal(struct sock *sk, u32 mss_now,
 				       int large_allowed)
 {
